624911789067e1731535284c146687d2dd765699
pinn.public <pinn.public@gmail.com>
2025-11-26 16:50:26 +0900

    edit

--- script.sql
+++ script.sql
@@ -1,22 +1,22 @@
--- [필수!] SYS 계정이 XEPDB1 플러그인 데이터베이스로 전환하도록 함
+-- SYS 계정이 XEPDB1 플러그인 데이터베이스로 전환하도록 함
 ALTER SESSION SET CONTAINER=XEPDB1;
 
--- [필수!] springboot 유저 생성
+-- springboot 유저 생성
 CREATE USER springboot IDENTIFIED BY java1234;
 
--- [수정!] ORA-01045 오류 해결: 'CREATE SESSION' 권한을 *명시적*으로 부여
+-- ORA-01045 오류 해결: 'CREATE SESSION' 권한을 *명시적*으로 부여
 GRANT CREATE SESSION TO springboot;
 
--- [수정!] 테이블 생성, 시퀀스 생성, 리소스 사용 권한 부여
+-- 테이블 생성, 시퀀스 생성, 리소스 사용 권한 부여
 GRANT RESOURCE TO springboot;
 GRANT CREATE TABLE TO springboot;
 GRANT CREATE VIEW TO springboot;
 GRANT CREATE SEQUENCE TO springboot;
 
--- [수정!] 테이블스페이스 용량 할당
+-- 테이블스페이스 용량 할당
 ALTER USER springboot DEFAULT TABLESPACE USERS QUOTA UNLIMITED ON USERS;
 
--- [수정] springboot 유저 소유로 테이블 생성
+-- springboot 유저 소유로 테이블 생성
 create table springboot.tblTeam (
     seq number primary key,
     name varchar2(100) not null unique,

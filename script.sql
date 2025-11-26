6ee2fe94a131570d483cb3bfc454745b479acf86
pinn.public <pinn.public@gmail.com>
2025-11-26 16:07:11 +0900

    edit

--- script.sql
+++ script.sql
@@ -1,3 +1,4 @@
+/*
 -- [필수!] SYS 계정이 XEPDB1 플러그인 데이터베이스로 전환하도록 함
 ALTER SESSION SET CONTAINER=XEPDB1;
 
@@ -30,5 +31,27 @@ insert into springboot.tblTeam values (springboot.seqTeam.nextVal, 'DB', '데이
 insert into springboot.tblTeam values (springboot.seqTeam.nextVal, '운영', '구축된 시스템의 유지보수를 담당합니다.');
 insert into springboot.tblTeam values (springboot.seqTeam.nextVal, '영업', '진행중인 프로젝트를 관리합니다.');
 insert into springboot.tblTeam values (springboot.seqTeam.nextVal, '보안', '시스템의 보안을 담당합니다.');
+
+commit;
+
+*/
+
+ALTER SESSION SET CONTAINER=XEPDB1;
+
+ALTER SESSION SET CURRENT_SCHEMA=springboot;
+
+create table tblTeam (
+    seq number primary key,
+    name varchar2(100) not null unique,
+    description varchar2(300) not null
+);
+
+create sequence seqTeam;
+
+insert into tblTeam values (seqTeam.nextVal, '개발', '시스템 개발을 담당합니다.');
+insert into tblTeam values (seqTeam.nextVal, 'DB', '데이터베이스를 담당합니다.');
+insert into tblTeam values (seqTeam.nextVal, '운영', '구축된 시스템의 유지보수를 담당합니다.');
+insert into tblTeam values (seqTeam.nextVal, '영업', '진행중인 프로젝트를 관리합니다.');
+insert into tblTeam values (seqTeam.nextVal, '보안', '시스템의 보안을 담당합니다.');
 
 commit;
